f7ed72de259854609ba95b19841d6500224f0220
TeenCoder <martin.junyoung@gmail.com>
2019-02-21 11:10:00 +0900

    js/css/html update 2

--- weather.js
+++ weather.js
@@ -4,7 +4,31 @@ const API_KEY = '88b38985502a5cd4e55297d3bfe8dcee',
     mainWeather = weatherDiv.querySelector('.main-weather'),
     temp = weatherDiv.querySelector(".temp"),
     minMaxTemp = weatherDiv.querySelector('.min-max-temp'),
-    humidity = weatherDiv.querySelector('.humidity');
+    humidity = weatherDiv.querySelector('.humidity'),
+    errorCont = weatherDiv.querySelector('.error-message');
+
+const KoreanPatch = [
+    {
+        weather: 'Haze',
+        description: '안개'
+    },
+    {
+        weather: 'Mist',
+        description: '적은 안개'
+    },
+    {
+        weather: 'Clouds',
+        description: '흐림'
+    },
+    {
+        weather: 'Rain',
+        description: '비'
+    },
+    {
+        weather: 'Clear',
+        description: '맑음'
+    }
+];
 
 let currentWeather = '';
 
@@ -29,6 +53,14 @@ function figureWeather(json){
     console.log(currentWeather);
     bodyContainer.classList = '';
     bodyContainer.classList.add(currentWeather);
+    const koreanDes = KoreanPatch.filter(koreanDes => koreanDes.weather === currentWeather);
+    console.log(koreanDes);
+    if (koreanDes[0].description !== null){
+        console.log(koreanDes[0].description);
+        mainWeather.innerHTML = `${koreanDes[0].description}`
+    } else {
+        errorCont.innerHTML = `undefined weather - ${json.weather[0].main} - 카페에 제보해 주세요^^`
+    }
 }
 
 function init(){
